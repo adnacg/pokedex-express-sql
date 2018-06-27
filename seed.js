d7aa87935bbbaabdce2104bbe044e808643d2860
adna <adna@adna.com>
2018-06-27 20:13:39 +0800

    WIP

--- seed.js
+++ seed.js
@@ -1,11 +1,9 @@
 const jsonfile = require('jsonfile');
 const FILE = 'pokedex.json';
 
-
-// REMEMBER TO CHANGE YOUR CONFIGS BEFORE RUNNING THIS SCRIPT!!
 const pg = require('pg');
 const configs = {
-	user: 'scottlee',
+	user: 'elvera',
 	host: '127.0.0.1',
 	database: 'pokemons',
 	port: '5432'
@@ -18,14 +16,10 @@ jsonfile.readFile(FILE, (err,obj)=>{
 		if (err) {
 			console.log('Connection error', err.message);
 		}
-
 		console.log('Connected to dB');
-
 		let text = 'INSERT INTO pokemon (name, num, img, weight, height) ' + ' VALUES($1, $2, $3, $4, $5) RETURNING *';
-
 		for (let i=0; i<obj.pokemon.length; i++){
 			let values = [obj.pokemon[i].name, obj.pokemon[i].num, obj.pokemon[i].img, obj.pokemon[i].weight, obj.pokemon[i].height];
-
 			client.query(text, values, (err, res) => {
 				if (err) {
 					console.log("Query error", err.message);
@@ -34,6 +28,6 @@ jsonfile.readFile(FILE, (err,obj)=>{
 				}
 				if (res.rows[0].id == 151) { client.end() };
 			});
-		}	
+		}
 	});
 });
